1bfbd79b732542a75008baf8b63f369aa48977ef
dongliangCai <79450676+dongliangCai@users.noreply.github.com>
2025-01-10 17:03:26 +0800

    update Cargo.toml

--- sp1-tests/test-bn254/program/src/main.rs
+++ sp1-tests/test-bn254/program/src/main.rs
@@ -19,38 +19,38 @@ fn main() {
         "cycle-tracker-end: hash_with_domain(&[Fr::from(1u64), Fr::from(2u64)], Fr::from(3u64))"
     );
 
-    let msgs = [
-        &array::from_fn::<_, 1, _>(|i| Fr::from(i as u64))[..],
-        &array::from_fn::<_, 10, _>(|i| Fr::from(i as u64))[..],
-        &array::from_fn::<_, 11, _>(|i| Fr::from(i as u64))[..],
-    ];
+    // let msgs = [
+    //     &array::from_fn::<_, 1, _>(|i| Fr::from(i as u64))[..],
+    //     &array::from_fn::<_, 10, _>(|i| Fr::from(i as u64))[..],
+    //     &array::from_fn::<_, 11, _>(|i| Fr::from(i as u64))[..],
+    // ];
 
-    let cap = [None, Some(100)];
+    // let cap = [None, Some(100)];
 
-    for (msg, cap) in iproduct!(msgs.iter(), cap.iter()) {
-        let tag = format!("hash_msg({}, {:?})", msg.len(), cap);
-        println!("{}", format!("cycle-tracker-start: {tag}"));
-        hash_msg(msg, *cap);
-        println!("{}", format!("cycle-tracker-end: {tag}"));
-    }
+    // for (msg, cap) in iproduct!(msgs.iter(), cap.iter()) {
+    //     let tag = format!("hash_msg({}, {:?})", msg.len(), cap);
+    //     println!("{}", format!("cycle-tracker-start: {tag}"));
+    //     hash_msg(msg, *cap);
+    //     println!("{}", format!("cycle-tracker-end: {tag}"));
+    // }
 
-    let codes = [
-        &[],
-        &array::from_fn::<_, 1, _>(|i| i as u8)[..],
-        &array::from_fn::<_, 128, _>(|i| i as u8)[..],
-        &array::from_fn::<_, 256, _>(|i| i as u8)[..],
-        &array::from_fn::<_, 512, _>(|i| i as u8)[..],
-        &array::from_fn::<_, 1024, _>(|i| i as u8)[..],
-        &array::from_fn::<_, 2048, _>(|i| i as u8)[..],
-        &array::from_fn::<_, 4096, _>(|i| i as u8)[..],
-        &array::from_fn::<_, 16384, _>(|i| i as u8)[..],
-        &array::from_fn::<_, 24576, _>(|i| i as u8)[..],
-    ];
+    // let codes = [
+    //     &[],
+    //     &array::from_fn::<_, 1, _>(|i| i as u8)[..],
+    //     &array::from_fn::<_, 128, _>(|i| i as u8)[..],
+    //     &array::from_fn::<_, 256, _>(|i| i as u8)[..],
+    //     &array::from_fn::<_, 512, _>(|i| i as u8)[..],
+    //     &array::from_fn::<_, 1024, _>(|i| i as u8)[..],
+    //     &array::from_fn::<_, 2048, _>(|i| i as u8)[..],
+    //     &array::from_fn::<_, 4096, _>(|i| i as u8)[..],
+    //     &array::from_fn::<_, 16384, _>(|i| i as u8)[..],
+    //     &array::from_fn::<_, 24576, _>(|i| i as u8)[..],
+    // ];
 
-    for code in codes.iter() {
-        let tag = format!("hash_code({})", code.len());
-        println!("{}", format!("cycle-tracker-start: {tag}"));
-        let result = hash_code(code);
-        println!("{}", format!("cycle-tracker-end: {tag}"));
-    }
+    // for code in codes.iter() {
+    //     let tag = format!("hash_code({})", code.len());
+    //     println!("{}", format!("cycle-tracker-start: {tag}"));
+    //     let result = hash_code(code);
+    //     println!("{}", format!("cycle-tracker-end: {tag}"));
+    // }
 }
